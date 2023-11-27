4b1af9844e711804090e22e09fa6ccc2e5fc917c
Evaldo-Gomes <evaldo_gomes@hotmail.com>
2023-11-27 13:20:50 -0300

    - Commit inicial

---
+++ jurosSimples.py
@@ -0,0 +1,11 @@
+# Calculadora de Juros Simples
+# Apresentação
+print('\n\t\t\t  -- Calculadora de Juros Simples --')
+# Entradas
+c = int(input('Informe o capital (R$): '))
+i = float(input('Informe a taxa (%): '))
+n = int(input('Informe a quantidade de parcelas: '))
+# Processamento
+j = (c*i*n)/100
+# Saída
+print(f'Juros: R$ {j}')
